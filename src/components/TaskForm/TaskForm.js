da700024f830b7f46b4133718b78d00b5a947991
Dmytro Kozlovskyi <dmytrokozlovskyi@gmail.com>
2021-11-11 14:54:00 +0200

    Added TaskForm comonent

---
+++ src/components/TaskForm/TaskForm.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import TaskTitleInput from "./TaskTitleInput/TaskTitleInput";
+import TaskTimeArea from "./TaskTimeArea/TaskTimeArea";
+import { connect } from "react-redux";
+import { addTask } from "../../redux/actions/taskActions";
+const StyledEventForm = styled.div``;
+
+function TaskForm({ addTask, taskTitle, autoTime, userTime }) {
+  return (
+    <div className="container">
+      <TaskTitleInput />
+      <TaskTimeArea />
+
+      <div className="row">
+        <div class="d-grid col-sm-12">
+          <button
+            class="btn btn-lg btn-primary"
+            type="button"
+            onClick={() => addTask(taskTitle, autoTime, userTime)}
+          >
+            Create task
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+const mapStateToProps = (state) => {
+  return {
+    tasksArr: state.tasks,
+    taskTitle: state.taskTitleInput.value,
+    ...state.taskTimeArea,
+  };
+};
+
+const mapDispatchToProps = { addTask };
+
+export default connect(mapStateToProps, mapDispatchToProps)(TaskForm);
