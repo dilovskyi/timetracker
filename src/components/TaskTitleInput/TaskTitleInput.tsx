13111498dba45a6ea0cb4651c62b0d3384063739
Dmytro Kozlovskyi <dmytrokozlovskyi@gmail.com>
2021-11-18 16:46:58 +0200

    Added TaskTitleInput validation

--- src/components/TaskTitleInput/TaskTitleInput.tsx
+++ src/components/TaskTitleInput/TaskTitleInput.tsx
@@ -1,27 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import * as actions from "../../redux/actions/taskTitleInput";
 import { addTask } from "../../redux/actions/taskList";
-import {StateType} from '../../redux/storeTypes'
+import { StateType } from "../../redux/storeTypes";
 
 interface StateProps {
-  value: string,
+  value: string;
 }
 
 interface DispatchProps {
-  setTaskTitle: (value: string) => void,
-  resetTaskTitle: () => void,
-  addTask: (title:string,id:string) => void,
+  setTaskTitle: (value: string) => void;
+  resetTaskTitle: () => void;
+  addTask: (title: string, id: string) => void;
 }
 
-type Props = StateProps & DispatchProps
+type Props = StateProps & DispatchProps;
 
-function TaskTitleInput ({ value, setTaskTitle, resetTaskTitle, addTask }:Props):JSX.Element {
-  const pressedInput = (e: React.KeyboardEvent<HTMLTextAreaElement>):void => {
+function TaskTitleInput({
+  value,
+  setTaskTitle,
+  resetTaskTitle,
+  addTask,
+}: Props): JSX.Element {
+  const [errorStatus, setErrorStatus] = useState(false);
+
+  const pressedInput = (e: React.KeyboardEvent<HTMLTextAreaElement>): void => {
     if (e.target && e.charCode === 13) {
       e.preventDefault();
-      addTask(value, new Date().getTime().toString());
-      resetTaskTitle();
+      if (value.length < 1) {
+        setErrorStatus(true);
+      } else {
+        addTask(value, new Date().getTime().toString());
+        resetTaskTitle();
+      }
     }
   };
 
@@ -30,21 +41,25 @@ function TaskTitleInput ({ value, setTaskTitle, resetTaskTitle, addTask }:Props)
       <div className="form-floating">
         <textarea
           id="floatingTextarea"
-          className="form-control col-12"
+          className={`form-control col-12 ${errorStatus ? "is-invalid" : ""}`}
           placeholder="Leave a task tittle here"
           autoFocus
           value={value}
           onKeyPress={pressedInput}
-          onChange={(e) => setTaskTitle(e.target.value)}
-        ></textarea>
-        <label htmlFor="floatingTextarea">Comments</label>
+          onChange={(e) => setTaskTitle(e.target.value)}></textarea>
+
+        {!errorStatus ? (
+          <label htmlFor="floatingTextarea">Comments</label>
+        ) : (
+          <label htmlFor="floatingTextarea">Invalid input</label>
+        )}
       </div>
     </>
   );
 }
 
-const mapStateToProps = (state:StateType) => {
-  return { ...state.taskTitleInput};
+const mapStateToProps = (state: StateType) => {
+  return { ...state.taskTitleInput };
 };
 
 const mapDispatchToProps = {
